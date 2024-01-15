4587075008a15d66cbbdd5d094a312c838390d64
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2024-01-15 23:51:51 +0530

    Update userRouter.js

--- Blogify/routes/userRouter.js
+++ Blogify/routes/userRouter.js
@@ -2,10 +2,12 @@ const express = require('express');
 const { createUser, loginUser, logoutUser } = require('../controllers/user.controller');
 const userRouter = express.Router();
 
-
+// user Login Route 
 userRouter.post('/login', loginUser)
 
+// user Sign Route
 userRouter.post('/signup', createUser)
 
+// user Logout Route
 userRouter.get('/logout', logoutUser)
 module.exports = userRouter
