bd52e14c72f6cf7ffe2a66898f41645c4251cd62
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2024-01-17 23:42:57 +0530

    Update blog.controller.js

--- Blogify/controllers/blog.controller.js
+++ Blogify/controllers/blog.controller.js
@@ -1,5 +1,6 @@
 const BLOG = require('../models/blog.model.js');
 const COMMENT = require('../models/comment.model.js');
+// Create Blog
 const createBlog = async (req, res) => {
     const { title, description } = req.body
     const blog = await BLOG.create({
@@ -10,13 +11,13 @@ const createBlog = async (req, res) => {
     })
     res.redirect('/')
 }
-
+// Delete Blog 
 const deleteBlog = async (req, res) => {
     const blog = await BLOG.findById(req.params.id)
     await blog.deleteOne();
     res.redirect('/profile')
 }
-
+// Create Comment
 const createComment = async (req, res) => {
     const comment = await COMMENT.create({
         content: req.body.content,
