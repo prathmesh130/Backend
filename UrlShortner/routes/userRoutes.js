ee3065031f0ec9286696f4c24228cb5924c88cd4
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2024-02-06 23:56:59 +0530

    Update userRoutes.js

--- UrlShortner/routes/userRoutes.js
+++ UrlShortner/routes/userRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const { createUser, loginUser,logoutUser } = require('../controllers/user.controller');
 const userRouter = express.Router();
 
+// Singup route
 userRouter.post('/signup', createUser)
+// Login Route
 userRouter.post('/login', loginUser)
+// logout route
 userRouter.get('/logout', logoutUser)
 module.exports = userRouter
