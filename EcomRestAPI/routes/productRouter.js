af29404a89ba255635e48ef0edfcce1b597e40df
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2024-02-22 23:56:16 +0530

    Update productRouter.js

--- EcomRestAPI/routes/productRouter.js
+++ EcomRestAPI/routes/productRouter.js
@@ -4,9 +4,13 @@ import { auth } from "../middlewares/auth";
 import admin from "../middlewares/admin";
 import upload from "../middlewares/upload";
 export const productRouter = express.Router();
-
+// post route for adding product
 productRouter.post('/', [auth, admin, upload.single('image')], productController.addProduct)
+// get all products
 productRouter.get('/', productController.getAllProducts)
+// get single product 
 productRouter.get('/:id', productController.getProduct)
+// update product
 productRouter.put('/:id', [auth, admin], productController.updateProduct)
+// delete product
 productRouter.delete('/:id', [auth, admin], productController.deleteProduct)
