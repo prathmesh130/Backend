ca9e7e057f8b6428d2cb043d8f913eb990de5e36
Prathamesh Lade <64533147+prathmesh130@users.noreply.github.com>
2024-03-07 23:55:26 +0530

    Update index.js

--- Youtube/src/database/index.js
+++ Youtube/src/database/index.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { DB_NAME } from '../constants.js'
 
+// export database connection
 export const connectDB = async () => {
     try {
         const dbConnect = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
